c07e46bb111017e7e2b4d5d3c8d8e71626974da4
Mike DeLong <mikedelong@outlook.com>
2022-12-13 14:59:00 -0500

    feature: first working version; shows the lmplot and plots hours vs. age.

--- terrapin_mountain.py
+++ terrapin_mountain.py
@@ -1,5 +1,5 @@
 """
-Download NASA data from the Princeton site
+Scatter plot of age vs finishing tome
 """
 
 from logging import INFO
@@ -8,17 +8,37 @@ from logging import getLogger
 from pathlib import Path
 
 from arrow import now
+from matplotlib.pyplot import Axes
+from matplotlib.pyplot import close
+from matplotlib.pyplot import gca
+from matplotlib.pyplot import savefig
+from matplotlib.pyplot import subplots
+from matplotlib.pyplot import tight_layout
+from pandas import Series
+from pandas import concat
 from pandas import read_csv
+from seaborn import lmplot
+from seaborn import set_style
 
 
 def get_hours(arg: str) -> float:
     pieces = arg.split(':')
     seconds = 3600 * int(pieces[0]) + 60 * int(pieces[1]) + float(pieces[2])
-    return seconds/3600
+    return seconds / 3600
+
+
+# https://stackoverflow.com/questions/46027653/adding-labels-in-x-y-scatter-plot-with-seaborn
+def label_point(x: Series, y: Series, val: Series, ax: Axes):
+    rows_df = concat({'x': x, 'y': y, 'value': val}, axis=1)
+    for i, point in rows_df.iterrows():
+        ax.text(point['x'] + 0.03, point['y'] + 0.01, str(point['value']), fontsize='x-small')
+    return
+
 
 INPUT_FILE = '2022 Terrapin 50K_.csv'
 INPUT_FOLDER = './data/'
 OUTPUT_FOLDER = './plot/'
+SEABORN_STYLE = 'darkgrid'
 
 if __name__ == '__main__':
     TIME_START = now()
@@ -35,5 +55,17 @@ if __name__ == '__main__':
     LOGGER.info(df.shape)
     LOGGER.info(df.columns.tolist())
     df['Hours'] = df['Chip Time'].apply(get_hours)
+
+    set_style(style=SEABORN_STYLE)
+
+    figure_scatterplot, axes_scatterplot = subplots()
+    result_scatterplot = lmplot(data=df, x='Age', y='Hours',
+                                # hue='hue',
+                                legend=False, aspect=2, )
+    label_point(x=df['Age'], y=df['Hours'], val=df['Name'], ax=gca())
+    tight_layout()
+
+    savefig(fname=OUTPUT_FOLDER + '{}_scatterplot.png'.format('terrapin_mountain_2022'), format='png')
+    close(fig=figure_scatterplot)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
