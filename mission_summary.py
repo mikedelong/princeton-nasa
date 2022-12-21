3e18b93d67533c2f238055ac0dbaa2f322917db1
Mike DeLong <mikedelong@outlook.com>
2022-12-21 18:54:09 -0500

    feature: roll up the dollars/year calculation into a lambda

--- mission_summary.py
+++ mission_summary.py
@@ -44,10 +44,8 @@ if __name__ == '__main__':
     use(style=STYLE)
     y_coordinate = 10
     y_ticks = list()
+    df['$/Year'] = df.apply(axis=1 , func=lambda x: 0 if x['End'] == x['Launch'] else x['2019 M Total'] / (x['End'] - x['Launch']))
     for index, row in df.iterrows():
-        dollars = row['2019 M Total']
-        years = row['End'] - row['Launch']
-        dollars_per_year = dollars/years if years > 0 else 0
         facecolor = 'C0' if row['End'] != 2019 else 'C2'
         ax.broken_barh([(row['Launch'], row['End'] - row['Launch'])], (y_coordinate, 9), facecolor=facecolor)
         y_ticks.append(y_coordinate + 5)
