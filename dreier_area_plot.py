93ea1399f38d1b11c84c04083f5c984e1dc85c89
Mike DeLong <mikedelong@outlook.com>
2023-01-05 18:40:09 -0500

    feature: get the mission costs sheet as a DataFrame

--- dreier_area_plot.py
+++ dreier_area_plot.py
@@ -8,7 +8,14 @@ from logging import getLogger
 from pathlib import Path
 
 from arrow import now
+from pandas import DataFrame
 from pandas import ExcelFile
+from pandas import read_excel
+
+
+def read_dataframe_excel(arg: str, sheet_name: str) -> DataFrame:
+    result_df = read_excel(io=arg, sheet_name=sheet_name, engine='openpyxl')
+    return result_df
 
 
 INPUT_FILE = 'Planetary Exploration Budget Dataset - The Planetary Society.xlsx'
@@ -29,5 +36,8 @@ if __name__ == '__main__':
     xl = ExcelFile(path_or_buffer=input_file, engine=engine)
     sheet_names = xl.sheet_names
     LOGGER.info(sheet_names)
+
+    df = read_dataframe_excel(arg=input_file, sheet_name='Mission Costs')
+    LOGGER.info(df.shape)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
